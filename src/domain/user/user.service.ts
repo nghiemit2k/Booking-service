f045bd77340897fb2c8ddb1662519d235dfefac0
nghiem <nghiem066@gmail.com>
2024-06-27 20:27:17 +0700

    feat(Prisma): integrate prisma

--- src/domain/user/user.service.ts
+++ src/domain/user/user.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from "@nestjs/common";
-
+import { Prisma } from "@prisma/client";
+import { DatabaseService } from "src/database/database.service";
 @Injectable()
 
 export class UserService {
-    register(data: any) {
-        console.log('register',data);
+    constructor(private databaseService: DatabaseService) {};
+    findMany() {
+        return this.databaseService.user.findMany();
+    }
+    register(data: Prisma.UserCreateInput) {
+        const userData ={
+            email: data.email,
+            password:  data.password,
+            firstName: data.firstName,
+            lastName: data.lastName,
+            phone: data.phone,
+            timezoneCode: data.timezoneCode
+
+        };
+        return this.databaseService.user.create({data: userData});
     }
 }
