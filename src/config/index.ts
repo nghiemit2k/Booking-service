ed62ee8a5952e6e55c4d56e623590c122ca9423b
nghiem <nghiem066@gmail.com>
2024-07-13 12:10:46 +0700

    feat(Config): implement env var configs

---
+++ src/config/index.ts
@@ -0,0 +1,56 @@
+// validate the environment variable: class-validator
+// export environment variable
+
+import { plainToInstance } from "class-transformer";
+import { IsNotEmpty, IsOptional, validateSync } from "class-validator";
+
+export class EnvironmentVariables{
+    @IsOptional()
+    APP_NAME?:string;
+
+    @IsOptional()
+    APP_PORT?: number;
+
+    @IsNotEmpty()
+    DATABASE_HOST: string;
+
+    @IsNotEmpty()
+    DATABASE_PORT:number;
+
+    @IsNotEmpty()
+    DATABASE_USERNAME: string;
+
+    @IsNotEmpty()
+    DATABASE_PASSWORD: string;
+
+    @IsNotEmpty()
+    DATABASE_NAME: string;
+    
+    
+}
+
+export function validate(config: Record<string,unknown>) {
+    const validateConfig = plainToInstance(EnvironmentVariables, config, {
+        enableImplicitConversion: true,
+    })
+    
+    const errors = validateSync(validateConfig, {
+        skipMissingProperties: false,
+    })
+
+    if(errors.length > 0) {
+        throw new Error(errors.toString())
+    }
+    return validateConfig;
+}
+const config = {
+    APP_NAME: process.env.APP_NAME || 'booking-service',
+    APP_PORT: +process.env.APP_PORT || 3000,
+    DATABASE_HOST: process.env.DATABASE_HOST,
+    DATABASE_PORT: +process.env.DATABASE_PORT,
+    DATABASE_USERNAME: process.env.DATABASE_USERNAME,
+    DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
+    DATABASE_NAME: process.env.DATABASE_NAME,
+}
+
+export default config;
