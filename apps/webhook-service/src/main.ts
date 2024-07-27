04a5c911dbab83a5b2724e4c62cc3462d2846ceb
nghiem <nghiem066@gmail.com>
2024-07-27 15:19:40 +0700

    feat: implement setUpApplication

--- apps/webhook-service/src/main.ts
+++ apps/webhook-service/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { WebhookServiceModule } from './webhook-service.module';
+import { AppModule } from 'apps/booking/src/app.module';
+import setUpApplication from '@libs/@libs/core/setup';
 
 async function bootstrap() {
+  
   const app = await NestFactory.create(WebhookServiceModule);
-  await app.listen(3000);
+
+  const { port, logInfo } = setUpApplication(app);
+
+  await app.listen(port);
+  logInfo();
 }
 bootstrap();
